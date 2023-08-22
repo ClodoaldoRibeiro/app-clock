69bc606ff923161813813cf566260ff97dce68d5
Clodoaldo Ribeiro <clodoaldorib@hotmail.com>
2023-08-22 09:13:36 -0300

    Place the battery level indicator

--- app/src/main/java/com/example/clockapp/MainActivity.java
+++ app/src/main/java/com/example/clockapp/MainActivity.java
@@ -2,11 +2,23 @@ package com.example.clockapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private ViewHolder mViewHolder = new ViewHolder();
+    public BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+            mViewHolder.textBattery.setText(String.format("Baterry level %s%%", level));
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
         mViewHolder.textHourMinutes = findViewById(R.id.text_hour_minute);
         mViewHolder.textSeconds = findViewById(R.id.text_seconds);
         mViewHolder.textBattery = findViewById(R.id.text_battery);
+
+
+        this.registerReceiver(mReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
     }
 
 
