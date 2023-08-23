500c67a8bb07e494acf9a52c95a67a701e1989ab
Clodoaldo Ribeiro <clodoaldorib@hotmail.com>
2023-08-23 10:51:10 -0300

    Update time activity and xml

--- app/src/main/java/com/example/clockapp/MainActivity.java
+++ app/src/main/java/com/example/clockapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.clockapp;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.BroadcastReceiver;
@@ -8,14 +9,23 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.BatteryManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.widget.TextView;
 
+import java.util.Calendar;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
+    private Runnable mRunnable;
     private ViewHolder mViewHolder = new ViewHolder();
+    final Handler mHandler = new Handler();
+
     public BroadcastReceiver mReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+
             mViewHolder.textBattery.setText(String.format("Baterry level %s%%", level));
         }
     };
@@ -29,8 +39,35 @@ public class MainActivity extends AppCompatActivity {
         mViewHolder.textSeconds = findViewById(R.id.text_seconds);
         mViewHolder.textBattery = findViewById(R.id.text_battery);
 
+        this.registerReceiver(this.mReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+        this.startClock();
+    }
 
-        this.registerReceiver(mReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+    private void startClock() {
+        Calendar calendar = Calendar.getInstance();
+        this.mRunnable = new Runnable() {
+            @Override
+            public void run() {
+                calendar.setTimeInMillis(System.currentTimeMillis());
+
+                int hour = calendar.get(Calendar.HOUR_OF_DAY);
+                int minute = calendar.get(Calendar.MINUTE);
+                int second = calendar.get(Calendar.SECOND);
+
+                mViewHolder.textHourMinutes.setText(
+                        String.format(Locale.getDefault(), "%02d:%02d", hour, minute)
+                );
+
+                mViewHolder.textSeconds.setText(
+                        String.format(Locale.getDefault(), "%02d", second)
+                );
+
+                long now = SystemClock.uptimeMillis();
+                long next = now + (1000 - (now % 1000));
+                mHandler.postAtTime(mRunnable, next);
+            }
+        };
+        this.mRunnable.run();
     }
 
 
