a3aa7c46b4d46ab261cc4453f471a702d2532581
Clodoaldo Ribeiro <clodoaldorib@hotmail.com>
2023-08-24 10:35:55 -0300

    Stop background processing

--- app/src/main/java/com/example/clockapp/MainActivity.java
+++ app/src/main/java/com/example/clockapp/MainActivity.java
@@ -11,6 +11,7 @@ import android.os.BatteryManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.SystemClock;
+import android.view.View;
 import android.widget.TextView;
 
 import java.util.Calendar;
@@ -20,6 +21,52 @@ public class MainActivity extends AppCompatActivity {
     private Runnable mRunnable;
     private ViewHolder mViewHolder = new ViewHolder();
     final Handler mHandler = new Handler();
+    private boolean mTicker;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        mViewHolder.textHourMinutes = findViewById(R.id.text_hour_minute);
+        mViewHolder.textSeconds = findViewById(R.id.text_seconds);
+        mViewHolder.textBattery = findViewById(R.id.text_battery);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        this.mTicker = true;
+        this.startClock();
+        this.registerReceiver(this.mReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        this.mTicker = false;
+        this.unregisterReceiver(mReceiver);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_IMMERSIVE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+            );
+        }
+    }
 
     public BroadcastReceiver mReceiver = new BroadcastReceiver() {
         @Override
@@ -30,24 +77,15 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        mViewHolder.textHourMinutes = findViewById(R.id.text_hour_minute);
-        mViewHolder.textSeconds = findViewById(R.id.text_seconds);
-        mViewHolder.textBattery = findViewById(R.id.text_battery);
-
-        this.registerReceiver(this.mReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
-        this.startClock();
-    }
-
     private void startClock() {
         Calendar calendar = Calendar.getInstance();
         this.mRunnable = new Runnable() {
             @Override
             public void run() {
+                if (!mTicker) {
+                    return;
+                }
+
                 calendar.setTimeInMillis(System.currentTimeMillis());
 
                 int hour = calendar.get(Calendar.HOUR_OF_DAY);
