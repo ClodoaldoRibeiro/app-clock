0a20be5415c43e383efac3f2e5720af58862493b
Clodoaldo Ribeiro <clodoaldorib@hotmail.com>
2023-08-25 22:57:30 -0300

    Definir cores para o modo noturno

--- app/src/main/java/com/example/clockapp/MainActivity.java
+++ app/src/main/java/com/example/clockapp/MainActivity.java
@@ -39,6 +39,22 @@ public class MainActivity extends AppCompatActivity {
         mViewHolder.textBattery = findViewById(R.id.text_battery);
         mViewHolder.textNight = findViewById(R.id.text_night);
 
+
+        mViewHolder.textHourMinutes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showSystemUI();
+            }
+        });
+
+    }
+
+    private void showSystemUI() {
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        );
     }
 
     @Override
